acab473eae9c9a551ea4b1002732104a48c0f87a
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-09 19:49:54 +0400

    pic

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import random
 app = FastAPI(
     title = "HotBeverageAPI",
     description="Gets a HotBeverage from the database. \n This what made by Michael twitter = https://twitter.com/Michaelrbparker ",
+    version="0.1.0",
     license_info={
         "name": "MIT",
         "url": "https://opensource.org/licenses/MIT",
